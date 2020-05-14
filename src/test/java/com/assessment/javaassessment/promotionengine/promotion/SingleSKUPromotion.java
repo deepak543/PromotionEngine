1faf12b43e0741eea7be8aaa9880e9c46339b572
deepakselvakumar <dd.deepak06@gmail.com>
2020-05-14 22:21:30 +0530

    created promotion for n items of an sku for a price

---
+++ src/test/java/com/assessment/javaassessment/promotionengine/promotion/SingleSKUPromotion.java
@@ -0,0 +1,32 @@
+package com.assessment.javaassessment.promotionengine.promotion;
+
+import com.assessment.javaassessment.promotionengine.model.CartItem;
+import com.assessment.javaassessment.promotionengine.model.ItemSKU;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+@Builder
+@Getter
+@Setter
+public class SingleSKUPromotion implements IPromotion{
+    ItemSKU itemSKU;
+    int quantity;
+    int price;
+    @Override
+    public boolean appliesTo(CartItem... cartItem) {
+
+        return cartItem[0].getItemSKU().getSKU().equals(this.itemSKU.getSKU())
+                && cartItem[0].getQuantity() >= this.quantity;
+
+    }
+
+    @Override
+    public int calculatePrice(CartItem... cartItem) {
+        if(!appliesTo(cartItem))
+            return -1;
+        int discountedPrice = price + ((cartItem[0].getQuantity() - this.quantity) * cartItem[0].getItemSKU().getPrice());
+        return discountedPrice;
+    }
+}
