9ef670abb848130b9a485b2ec7d33cce290fe88e
deepakselvakumar <dd.deepak06@gmail.com>
2020-05-14 17:50:37 +0530

    Initial commit

---
+++ src/test/java/com/assessment/javaassessment/promotionengine/PromotionEngineTest.java
@@ -0,0 +1,24 @@
+package com.assessment.javaassessment.promotionengine;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+class PromotionEngineTest {
+
+    @BeforeAll
+    public void setUp()
+    {
+        List<ItemSKU> itemSKUList = new ArrayList<>();
+        itemSKUList.add(ItemSKU.builder().SKU("A").price(50).build());
+        itemSKUList.add(ItemSKU.builder().SKU("B").price(30).build());
+        itemSKUList.add(ItemSKU.builder().SKU("C").price(20).build());
+        itemSKUList.add(ItemSKU.builder().SKU("D").price(15).build());
+
+        Inventory inventory = new Inventory();
+        inventory.setup(itemSKUList);
+    }
+
+}
