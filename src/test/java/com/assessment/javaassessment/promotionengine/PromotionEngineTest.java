86f2a79eb0786cd1e81f267f2ed21444b82bf2fb
deepakselvakumar <dd.deepak06@gmail.com>
2020-05-14 20:13:02 +0530

    added basic single sku promotion and test case

--- src/test/java/com/assessment/javaassessment/promotionengine/PromotionEngineTest.java
+++ src/test/java/com/assessment/javaassessment/promotionengine/PromotionEngineTest.java
@@ -1,7 +1,12 @@
 package com.assessment.javaassessment.promotionengine;
+import com.assessment.javaassessment.promotionengine.model.CartItem;
 import com.assessment.javaassessment.promotionengine.model.ItemSKU;
+import com.assessment.javaassessment.promotionengine.promotion.IPromotion;
+import com.assessment.javaassessment.promotionengine.promotion.SingleSKUPromotion;
 import com.assessment.javaassessment.promotionengine.service.Inventory;
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +16,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 class PromotionEngineTest {
 
     @BeforeAll
-    public void setUp()
+    public static void setUp()
     {
         List<ItemSKU> itemSKUList = new ArrayList<>();
         itemSKUList.add(ItemSKU.builder().SKU("A").price(50).build());
@@ -21,6 +26,34 @@ class PromotionEngineTest {
 
         Inventory inventory = new Inventory();
         inventory.setup(itemSKUList);
+    }
+
+    @Test
+    public void cartItemTest()
+    {
+        CartItem cartItem = CartItem.builder().itemSKU(ItemSKU.builder().price(50).SKU("A").build()).quantity(2).build();
+        assertThat(cartItem.getItemSKU().getSKU(), Is.is("A"));
+        assertThat(cartItem.getQuantity(), Is.is(2));
+        assertThat(cartItem.getItemSKU().getPrice(), Is.is(50));
+    }
+
+    @Test
+    public void singleSKUPromotionAppliesToTest()
+    {
+        IPromotion promotion =  SingleSKUPromotion.builder()
+                .itemSKU(ItemSKU.builder().SKU("A").price(50).build())
+                .quantity(3)
+                .price(150)
+                .build();
+
+        CartItem cartItem = CartItem.builder()
+                .itemSKU(ItemSKU.builder()
+                        .price(50)
+                        .SKU("A")
+                        .build())
+                .quantity(5)
+                .build();
+        assertTrue(promotion.appliesTo(cartItem));
     }
 
 }
